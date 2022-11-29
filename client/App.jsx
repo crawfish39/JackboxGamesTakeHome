5dcb3b756bc906726a4717e22c80ebae8585e955
Nathan_Crawford <nathanr.crawford39@gmail.com>
2022-11-29 08:06:27 -0500

    initial finished app

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, {Component} from "react";
 import Grid from "./components/Grid.jsx";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import DragDrop from "./components/DragDrop.jsx";
+import pics from './imageScraper/images.json';
 
-function App(){
-    return(
-        <div>
-            <h1>Tier List Application</h1>
-            <Grid/>
-        </div>
-    )
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            filledBoxes: new Array(20).fill('-'),
+            unmovedPics: pics
+        };
+        
+        this.movePics = this.movePics.bind(this);
+    }
+
+    movePics(deleteIndex,imageId){
+        for(const image in this.state.unmovedPics){
+            if(this.state.unmovedPics[image].id === imageId){
+                const newUnmovedPics = [...this.state.unmovedPics.slice(0,imageId),...this.state.unmovedPics.slice(imageId+1)]
+                const newFilledBoxes = this.state.filledBoxes
+                return this.setState({
+                    unmovedPics: newUnmovedPics,
+                    filledBoxes: newFilledBoxes[deleteIndex] = pics[imageId]
+                })
+            }
+        }
+    }
+    
+
+    render(){
+        return(
+            <DndProvider backend={HTML5Backend}>
+                <div>
+                    <DragDrop unmovedPics={this.state.unmovedPics}/>
+                    <h1>Tier List Application</h1>
+                    <Grid
+                        boxKeys={this.state.boxKeys}
+                        filledBoxes={this.state.filledBoxes}
+                        unmovedPics={this.state.unmovedPics}
+                        movePics={this.movePics}
+                    />
+                </div>
+            </DndProvider>
+        )
+        
+        }
 }
 
 export default App;
--- client/components/Row.jsx
+++ client/components/Row.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Box from "./Box.jsx";
 
 export default function Row(props) {
-    const boxes = Array.from(new Array(5), (el,i) => {
-         return <Box key={i} rowI={props.rowI} boxKeys ={props['boxKeys'][i]}></Box>
-            
-    });
+
+    const boxes = Array.from(new Array(5).fill(''), (el,i) => {
+        const cell = i + parseInt(props.rowI)*5;
+         return <Box
+            key={i}
+            cell={cell}
+            movePics={props.movePics}
+            filledBoxes={props.filledBoxes}
+        ></Box>
+    }); 
 
     return (
         <>
-            {boxes}
+            {boxes}   
         </>
     )
 }
